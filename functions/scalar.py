77df64840a0101577f514cc7b3ca613420ab45c6
frolikk <roadto3kmmr@gmail.com>
2019-09-06 16:58:36 +0700

    Scalar function parser done

--- functions/scalar.py
+++ functions/scalar.py
@@ -1,28 +1,34 @@
+import sympy
 from sympy.parsing.sympy_parser import (parse_expr,
                                         standard_transformations,
                                         implicit_application,
                                         implicit_multiplication,
-                                        split_symbols)
-from sympy.abc import *
-from math import exp
+                                        convert_xor)
+from math import log10, e
 
 class ScalarFunction:
     """Класс, инкапсулирующий вычисление скалярной функции, заданной в виде строки
 определенного формата"""
+    x = sympy.Symbol('x')
     def __init__(self, expr):
         # преобразования для парсера sympy
         transformations = standard_transformations + (implicit_multiplication, # умножение без знака умножения
-                                                      implicit_application,) # применение скалярных ф-ий без скобок
-        
-        expr = expr.replace('e', 'E')
-        expr = expr.replace('tg', 'tan')
-        expr = expr.replace('ctg', 'cot')
-        expr = expr.replace('^', '**')
+                                                      implicit_application,  # применение скалярных ф-ий без скобок
+                                                      convert_xor) # каретка как символ возведения в степень
 
+        # замены для парсера
+        replacers = {'e' : e,
+                     'tg' : sympy.functions.tan,
+                     'ctg' : sympy.functions.cot,
+                     'lg' : sympy.Lambda(x, sympy.functions.log(x) / sympy.functions.log(10))}
+        
+        expr = expr.lower()
+        
         self.fun = parse_expr(expr,
-                              transformations = transformations)
+                              transformations = transformations,
+                              local_dict=replacers)
         if self.fun.free_symbols - {x} != set():
             raise ValueError('Выражение содержит переменные кроме x')
         
     def __call__(self, arg):
-        return float(self.fun.subs({x : arg}))
+        return float(self.fun.subs({ScalarFunction.x : arg}))
