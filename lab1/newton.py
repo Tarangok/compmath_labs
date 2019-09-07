d6cfae95ab78ec66b3cfade28be1c45fc71a4bcc
frolikk <roadto3kmmr@gmail.com>
2019-09-07 11:52:02 +0700

    Added newton method

---
+++ lab1/newton.py
@@ -0,0 +1,23 @@
+def newton(f, a, b, eps):
+    def phi(f, x):
+        return x - f(x) / f(x, 1)
+    
+    if f(a) * f(a, 2) > 0:
+        x = a
+    elif f(b) * f(b, 2) > 0:
+        x = b
+    else:
+        c = a - f(a) * (b - a) / (f(b) - f(a))
+        if f(a) * f(c) < 0:
+            x = a
+        else:
+            x = c
+
+    xn = phi(f, x)
+    while abs(xn - x) > eps or abs(f(xn)) > eps:
+        x = xn
+        xn = phi(f, x)
+        print(x, xn, abs(xn-x), abs(f(xn)))
+
+    return xn, abs(xn - x), abs(f(xn))
+    
