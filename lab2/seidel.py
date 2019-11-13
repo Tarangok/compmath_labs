8daca35f04dabba00e90b1999f70876f60d28bc2
frolikk <roadto3kmmr@gmail.com>
2019-11-13 13:30:37 +0700

    Moved alpha/beta creat to functions

--- lab2/seidel.py
+++ lab2/seidel.py
@@ -8,7 +8,34 @@ from lab2.eps import norm
 # ненулевой элемент, то можно прибавить строку с этим эл-том
 # к строке с эл-том главной диагонали, чтобы получить экви-
 # валентную систему, но с ненулевым эл-том ГД.
-def seidel_norm(A: np.ndarray, b: np.ndarray, eps):
+
+def make_alpha(A: np.ndarray):
+    alpha = np.zeros(A.shape)
+    for i in range(A.shape[0]):
+        for j in range(A.shape[0]):
+            alpha[i, j] = 0.0 if i == j else -A[i, j] / A[i, i]
+    return alpha
+
+
+def make_beta(A, b: np.ndarray):
+    beta = np.zeros(b.shape[0])
+    for i in range(A.shape[0]):
+        beta[i] = b[i] / A[i, i]
+    return beta
+
+
+def test_diag(A: np.ndarray):
+    N = A.shape[0]
+    for i in range(N):
+        diag = abs(A[i, i])
+        nondiag = sum(abs(A[i, j]) for j in range(N) if i != j)
+        if diag <= nondiag:
+            return False
+    return True
+
+
+def solve(alpha: np.ndarray, beta: np.ndarray, b, eps):
+    '''Проводит итерационный процесс для решения СЛАУ'''
     def new_x(a, b, x):
         n = x.shape[0]
         ret = np.zeros(x.shape)
@@ -18,34 +45,11 @@ def seidel_norm(A: np.ndarray, b: np.ndarray, eps):
             ret[i] = b[i] + s1 + s2
         return ret
 
-    N = A.shape[0]
-    # Переходим от Ax = b к Cx = d,
-    # где C = A^T * A,
-    # d = A^T * b.
-    A_t = A.transpose()
-    C = A_t.dot(A)
-    d = A_t.dot(b)
-
-    for i in range(N):
-        diag = abs(C[i, i])
-        nondiag = sum(abs(C[i, j]) for j in range(N) if i != j)
-        if diag <= nondiag:
-            raise ValueError('Matrix not diagonally dominant')
-
     # начальное приближение
-    x = d.copy()
-
-    alpha = np.zeros(A.shape)
-    for i in range(A.shape[0]):
-        for j in range(A.shape[0]):
-            alpha[i, j] = 0.0 if i == j else -C[i, j] / C[i, i]
+    x = b.copy()
 
     n = norm(alpha)
     k_eps = (1 - n)/n * eps
-
-    beta = np.zeros(A.shape[0])
-    for i in range(A.shape[0]):
-        beta[i] = d[i] / C[i, i]
 
     xn = new_x(alpha, beta, x)
     n_diff = norm(xn - x)
@@ -54,4 +58,4 @@ def seidel_norm(A: np.ndarray, b: np.ndarray, eps):
         xn = new_x(alpha, beta, x)
         n_diff = norm(xn - x)
 
-    return alpha, beta, xn
+    return xn
