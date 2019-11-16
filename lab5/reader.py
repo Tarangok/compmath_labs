005c81d06a52e78089a661fa6c49becdd085cc7d
frolikk <roadto3kmmr@gmail.com>
2019-11-16 20:50:07 +0700

    Tweaked l5 task reader

--- lab5/reader.py
+++ lab5/reader.py
@@ -8,15 +8,14 @@ def read_task(fp: TextIO):
     der = int(fp.readline())
     n = int(fp.readline())
 
-    grid_type = fp.readline()[:1]
-    if grid_type == 'e':
+    t = fp.readline()[:1]
+    if t == 'e':
         a, b = tuple(float(r) for r in fp.readline().split(' '))
-        grid = make_eq_grid(a, b, n)
-    elif grid_type == 'n':
+        X = (a, b, n)
+    elif t == 'n':
         X = [float(r) for r in fp.readline().split(' ')]
         if len(X) != n+1:
             raise ValueError(f'Grid not of length {n+1}')
-        grid = NonequilateralGrid(X)
     else:
         raise ValueError('Unrecognized task type')
 
@@ -28,4 +27,4 @@ def read_task(fp: TextIO):
     f = fp.readline()
     F = ScalarFunction(f) if f != '' else None
 
-    return grid, Y, Xres, F, der
+    return t, X, Y, Xres, F, der
