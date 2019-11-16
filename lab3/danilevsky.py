7ca9e46775eb02d9f9c6a19971eb7fb14ff27dcf
frolikk <roadto3kmmr@gmail.com>
2019-11-16 20:49:10 +0700

    Added proper poly solver for real roots

--- lab3/danilevsky.py
+++ lab3/danilevsky.py
@@ -1,5 +1,8 @@
 import numpy as np
+from math import isclose
 from functions.polynomial import PolynomialFunction as Poly
+from lab3.aberth import aberth
+
 from functools import reduce
 
 
@@ -8,32 +11,37 @@ def get_frob_eigenvec(L, n):
     return np.asarray(list(L ** (n-i-1) for i in range(n)))
 
 
+def make_m(A, k):
+    if isclose(A[k+1, k], 0.0):
+        raise ValueError('Unable to build Frobenius mtx')
+    n = A.shape[0]
+    ret = np.ndarray(A.shape)
+    for i in range(n):
+        for j in range(n):
+            if i != k:
+                ret[i, j] = 0.0 if i != j else 1.0
+            elif j != k:
+                ret[i, j] = - A[k+1, j] / A[k+1, k]
+            else:
+                ret[i, j] = 1 / A[k+1, k]
+    return ret
+
+
+def make_m_inv(A, k):
+    n = A.shape[0]
+    ret = np.ndarray(A.shape)
+    for i in range(n):
+        for j in range(n):
+            if i != k:
+                ret[i, j] = 0.0 if i != j else 1.0
+            else:
+                ret[i, j] = A[k+1, j]
+    return ret
+
+
 def danilevsky(A: np.ndarray):
     '''Порождает м-цу Фробениуса P
 и м-цы M и M^-1'''
-    def make_m(A, k):
-        n = A.shape[0]
-        ret = np.ndarray(A.shape)
-        for i in range(n):
-            for j in range(n):
-                if i != k:
-                    ret[i, j] = 0.0 if i != j else 1.0
-                elif j != k:
-                    ret[i, j] = - A[k+1, j] / A[k+1, k]
-                else:
-                    ret[i, j] = 1 / A[k+1, k]
-        return ret
-
-    def make_m_inv(A, k):
-        n = A.shape[0]
-        ret = np.ndarray(A.shape)
-        for i in range(n):
-            for j in range(n):
-                if i != k:
-                    ret[i, j] = 0.0 if i != j else 1.0
-                else:
-                    ret[i, j] = A[k+1, j]
-        return ret
 
     n = A.shape[0]
     M = [None for i in range(n-1)]
@@ -52,6 +60,23 @@ def make_poly(P):
     '''Порождает полином из первой строки
 м-цы Фробениуса P'''
     return Poly([-p for p in reversed(P[0])] + [1.0])
+
+
+def solve_poly(F, eps=1e-4):
+    roots = [r.real for r in aberth(F, eps) if abs(r.imag) < eps]
+    roots.sort()
+    print(roots)
+
+    ret = []
+    i = 0
+    L = len(roots)
+    while i < L:
+        ret.append({'r': roots[i], 'k': 1})
+        i += 1
+        while i < L and abs(ret[-1]['r']-roots[i]) < eps:
+            i += 1
+            ret[-1]['k'] += 1
+    return ret
 
 
 def make_S(M):
