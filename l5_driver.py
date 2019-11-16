0e1a8f18ff76eaff4c20a0793d7563c23824ab15
frolikk <roadto3kmmr@gmail.com>
2019-11-16 20:50:32 +0700

    Finished the L5 driver

--- l5_driver.py
+++ l5_driver.py
@@ -1,26 +1,26 @@
-from lab5.lagrange import LagrangePolynomialN
+from lab5.lagrange import LagrangePolynomialN, LagrangePolynomialE
 from lab5.reader import read_task
-
-from interp.grid import EquilateralGrid, NonequilateralGrid
-from interp.function import GridFunction
 
 from sys import stdin, stdout
 from math import sqrt
 
 
-f_in = open('lab5/l5_test.txt', 'r')
+f_in = open('lab5/l5_test_neq.txt', 'r')
 f_out = stdout
 
 try:
-    X, Y, Xr, F, der = read_task(f_in)
-    L = LagrangePolynomialN(X, Y)
+    t, X, Y, Xr, F, der = read_task(f_in)
+    if t == 'n':
+        L = LagrangePolynomialN(X, Y)
+    else:
+        L = LagrangePolynomialE(*X, Y)
     Yr = [L(xr, der) for xr in Xr]
 
     for x, y in zip(Xr, Yr):
         f_out.write(f'{x} {y}\n')
 
     if F is not None:
-        std = sum(sqrt((F(x)-L(x)) ** 2) for x in X)  # или Xr
+        std = sum(sqrt((F(x)-L(x)) ** 2) for x in Xr)  # или Xr
         f_out.writelines(f'{std}\n')
 finally:
     if f_in is not stdin:
